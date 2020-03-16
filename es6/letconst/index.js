e4575679e9164e8b599c39dde93c1a5b1504d063
vishalavalani <vishal.avalani@gmail.com>
2020-03-16 10:04:16 +0530

    let const demo 1

---
+++ es6/letconst/index.js
@@ -0,0 +1,15 @@
+//Example 1
+//var
+const key = 'key';
+let points = 50;
+// var winner = false;
+
+if (points < 70) {
+  console.log('points', points);
+  //   console.log('winner', winner);
+  console.log('passed');
+  let winner = true;
+  //   console.log('winner', winner);
+}
+
+console.log('winner', winner);
