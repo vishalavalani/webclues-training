fd8571c6d5b0bfb4f261e852b13e2e693097ab35
vishalavalani <vishal.avalani@gmail.com>
2020-03-16 10:23:27 +0530

    demo 2

--- es6/letconst/index.js
+++ es6/letconst/index.js
@@ -19,3 +19,44 @@ if (points < 70) {
 }
 
 console.log('winner', winner);
+
+//Example 2
+
+let person1 = 'harsh';
+console.log('person name', person1);
+
+person1 = 'Devanshi';
+console.log('person name', person1);
+
+const person = {
+  name: 'Harsh',
+  age: 24
+};
+
+console.log('person name', person.name);
+console.log('person age', person.age);
+
+person.name = 'Devanshi';
+
+console.log('person name', person.name);
+console.log('person age', person.age);
+
+const config = {
+  url: 'https://www.vishalavalani.com',
+  googleToken: '123456678'
+};
+
+Object.freeze(config);
+console.log('config', config);
+config.url = 'https://abs.com';
+
+console.log('config', config);
+
+//Example 3 - Trivia
+
+for (let i = 0; i < 10; i++) {
+  console.log('counter', i);
+  setTimeout(function() {
+    console.log('timeout ' + i);
+  }, 1000);
+}
