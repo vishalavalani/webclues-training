4ebf61657e49997cb9e9b5273143e222d3d8d1a6
vishalavalani <vishal.avalani@gmail.com>
2020-03-17 08:47:55 +0530

    default export

---
+++ es6/importsexports/cricket.js
@@ -0,0 +1,3 @@
+export default function cricketScore(x, y) {
+  return x + y;
+}
--- es6/importsexports/index.js
+++ es6/importsexports/index.js
@@ -1,6 +1,10 @@
 import { person, calculateAverage, calculateRuns } from './lib.js';
+import * as library from './lib.js';
+import cricketScore from './cricket.js';
 
 console.log(calculateRuns(11, 22));
 console.log(calculateAverage(11, 22));
 
 console.log(person);
+
+console.log(cricketScore(1, 2));
