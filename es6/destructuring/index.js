442e8cf766815f758d8142802a6f800e2bfa2065
vishalavalani <vishal.avalani@gmail.com>
2020-03-17 12:08:41 +0530

    destructuring

--- es6/destructuring/index.js
+++ es6/destructuring/index.js
@@ -1,3 +1,4 @@
+//Destructuring
 const person = {
   id: 1,
   name: 'Devanshi',
@@ -38,3 +39,99 @@ const persons = [
 const [obj1, obj2, obj3] = persons;
 
 console.log(obj1, obj2, obj3);
+
+//loops
+
+const trainees = [
+  {
+    id: 1,
+    name: 'Devanshi',
+    designation: 'iOS Developer',
+    experience: 2,
+    skills: ['iOS'],
+    hobbies: ['travelling']
+  },
+  {
+    id: 2,
+    name: 'Parth',
+    designation: 'iOS Team Lead',
+    experience: 6,
+    skills: ['iOS', 'Leadership'],
+    hobbies: ['Cricket']
+  },
+  {
+    id: 3,
+    name: 'Yash',
+    designation: 'Android Developer',
+    experience: 5,
+    skills: ['Android', 'React Native'],
+    hobbies: ['Football', 'Video Games']
+  },
+  {
+    id: 4,
+    name: 'Avinash',
+    designation: 'iOS Developer',
+    experience: 4,
+    skills: ['iOS'],
+    hobbies: ['Cricket']
+  },
+  {
+    id: 5,
+    name: 'Suraj',
+    designation: 'Mobile Team Lead',
+    experience: 7,
+    skills: ['Android', 'Leadership', 'Python'],
+    hobbies: ['Video Games']
+  },
+  {
+    id: 6,
+    name: 'Amita',
+    designation: 'Android Developer',
+    experience: 4,
+    skills: ['Android'],
+    hobbies: ['Travelling']
+  },
+  {
+    id: 7,
+    name: 'Harsh',
+    designation: 'iOS Developer',
+    experience: 2,
+    skills: ['iOS'],
+    hobbies: ['Travelling', 'Programming']
+  },
+  {
+    id: 8,
+    name: 'Jayesh',
+    designation: 'iOS Developer',
+    experience: 5,
+    skills: ['iOS'],
+    hobbies: ['Cricket', 'Travelling', 'Programming']
+  },
+  {
+    id: 9,
+    name: 'Pankit',
+    designation: 'Tech Lead',
+    experience: 11,
+    skills: ['PHP', 'MVC Frameworks', 'Leadership'],
+    hobbies: ['Cricket']
+  }
+];
+
+for (const trainee of trainees) {
+  console.log(`${trainee.name} am enjoying training`);
+}
+
+for (const [index, trainee] of trainees.entries()) {
+  const { name } = trainee;
+  console.log(`id: ${index} ${name} am enjoying training`);
+}
+
+//forEach
+trainees.forEach((trainee, index) => {
+  const { name } = trainee;
+  console.log(`id: ${index} ${name} am enjoying training`);
+});
+
+//keys
+
+console.log(Object.keys(person));
