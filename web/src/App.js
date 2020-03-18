76ea191c3a5b1eb5cd1c5466d2edcd93dfbea588
vishalavalani <vishal.avalani@gmail.com>
2020-03-18 09:55:38 +0530

    refactoring

--- web/src/App.js
+++ web/src/App.js
@@ -1,94 +1,18 @@
 import React from 'react';
 import './App.css';
-
-const trainees = [
-  {
-    id: 1,
-    name: 'Devanshi',
-    designation: 'iOS Developer',
-    experience: 2,
-    skills: ['iOS'],
-    hobbies: ['travelling']
-  },
-  {
-    id: 2,
-    name: 'Parth',
-    designation: 'iOS Team Lead',
-    experience: 6,
-    skills: ['iOS', 'Leadership'],
-    hobbies: ['Cricket']
-  },
-  {
-    id: 3,
-    name: 'Yash',
-    designation: 'Android Developer',
-    experience: 5,
-    skills: ['Android', 'React Native'],
-    hobbies: ['Football', 'Video Games']
-  },
-  {
-    id: 4,
-    name: 'Avinash',
-    designation: 'iOS Developer',
-    experience: 4,
-    skills: ['iOS'],
-    hobbies: ['Cricket']
-  },
-  {
-    id: 5,
-    name: 'Suraj',
-    designation: 'Mobile Team Lead',
-    experience: 7,
-    skills: ['Android', 'Leadership', 'Python'],
-    hobbies: ['Video Games']
-  },
-  {
-    id: 6,
-    name: 'Amita',
-    designation: 'Android Developer',
-    experience: 4,
-    skills: ['Android'],
-    hobbies: ['Travelling']
-  },
-  {
-    id: 7,
-    name: 'Harsh',
-    designation: 'iOS Developer',
-    experience: 2,
-    skills: ['iOS'],
-    hobbies: ['Travelling', 'Programming']
-  },
-  {
-    id: 8,
-    name: 'Jayesh',
-    designation: 'iOS Developer',
-    experience: 5,
-    skills: ['iOS'],
-    hobbies: ['Cricket', 'Travelling', 'Programming']
-  },
-  {
-    id: 9,
-    name: 'Pankit',
-    designation: 'Tech Lead',
-    experience: 11,
-    skills: ['PHP', 'MVC Frameworks', 'Leadership'],
-    hobbies: ['Cricket']
-  }
-];
+import { trainees } from './shared/trainees';
 function App() {
-  return (
-    <div className="App">
-      {trainees.map((trainee, index) => {
-        const { name } = trainee;
-        return (
-          <div>
-            <span>{index + 1}. </span>
-            {name}
-          </div>
-        );
-      })}
-    </div>
-  );
+  const traineeObj = trainees.map((trainee, index) => {
+    const { name } = trainee;
+    return (
+      <div className="item">
+        <span>{index + 1}. </span>
+        <span className="name">{name}</span>
+      </div>
+    );
+  });
+  console.log('traineeObj', traineeObj);
+  return <div className="App">{traineeObj}</div>;
 }
 
 export default App;
---
+++ web/src/shared/trainees.js
@@ -0,0 +1,74 @@
+export const trainees = [
+  {
+    id: 1,
+    name: 'Devanshi',
+    designation: 'iOS Developer',
+    experience: 2,
+    skills: ['iOS'],
+    hobbies: ['travelling']
+  },
+  {
+    id: 2,
+    name: 'Parth',
+    designation: 'iOS Team Lead',
+    experience: 6,
+    skills: ['iOS', 'Leadership'],
+    hobbies: ['Cricket']
+  },
+  {
+    id: 3,
+    name: 'Yash',
+    designation: 'Android Developer',
+    experience: 5,
+    skills: ['Android', 'React Native'],
+    hobbies: ['Football', 'Video Games']
+  },
+  {
+    id: 4,
+    name: 'Avinash',
+    designation: 'iOS Developer',
+    experience: 4,
+    skills: ['iOS'],
+    hobbies: ['Cricket']
+  },
+  {
+    id: 5,
+    name: 'Suraj',
+    designation: 'Mobile Team Lead',
+    experience: 7,
+    skills: ['Android', 'Leadership', 'Python'],
+    hobbies: ['Video Games']
+  },
+  {
+    id: 6,
+    name: 'Amita',
+    designation: 'Android Developer',
+    experience: 4,
+    skills: ['Android'],
+    hobbies: ['Travelling']
+  },
+  {
+    id: 7,
+    name: 'Harsh',
+    designation: 'iOS Developer',
+    experience: 2,
+    skills: ['iOS'],
+    hobbies: ['Travelling', 'Programming']
+  },
+  {
+    id: 8,
+    name: 'Jayesh',
+    designation: 'iOS Developer',
+    experience: 5,
+    skills: ['iOS'],
+    hobbies: ['Cricket', 'Travelling', 'Programming']
+  },
+  {
+    id: 9,
+    name: 'Pankit',
+    designation: 'Tech Lead',
+    experience: 11,
+    skills: ['PHP', 'MVC Frameworks', 'Leadership'],
+    hobbies: ['Cricket']
+  }
+];
